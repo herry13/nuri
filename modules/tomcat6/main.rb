c6db27370662ada5c08d7ead0c1d8666a31d3fd3
Herry <herry@hpvm17.inf.ed.ac.uk>
2012-06-25 01:21:13 +0100

    update module Tomcat6 to be compatible with SL6.2

--- modules/tomcat6/main.rb
+++ modules/tomcat6/main.rb
@@ -15,41 +15,30 @@ module Tomcat6
 		def getState
 			os = self.getPlatform()
 			if os == 'sl'
-=begin
 				# installed & running
-				data = `/bin/rpm -qa httpd` if os == 'sl'
-				@state["installed"] = ( (data =~ /httpd/) != nil )
+				data = `/bin/rpm -qa tomcat6`
+				@state["installed"] = ((data =~ /tomcat6/) != nil)
 				if @state["installed"]
-					data = `/sbin/service httpd status`
-					@state["running"] = ( (data =~ /.*running.*/) != nil )
+					data = `/usr/bin/sudo /sbin/service tomcat6 status`
+					@state["running"] = ((data =~ /running/) != nil)
 				else
 					@state["running"] = false
 				end
-				# port
-				data = (File.file?("/etc/httpd/conf/httpd.conf") ? `/bin/grep -e "^Listen " /etc/httpd/conf/httpd.conf` : "")
-				if data.length > 0
-					data = data.split(' ')
-					@state["port"] = data[1].to_i
-				else
-					@state["port"] = 80
-				end
-=end
-
 			elsif os == 'ubuntu'
 				# installed & running
 				data = `/usr/bin/dpkg-query -W tomcat6`
 				@state["installed"] = ((data =~ /tomcat6/) != nil)
 				if @state["installed"]
-					data = `/usr/bin/sudo /usr/bin/service tomcat6 status`
+					data = `/usr/bin/sudo /usr/sbin/service tomcat6 status`
 					@state["running"] = ((data =~ /running/) != nil)
 				else
 					@state["running"] = false
 				end
-				# port
-				doc = XML::Parser.file('/etc/tomcat6/server.xml').parse
-				node = doc.find_first('/Server/Service/Connector')
-				@state["port"] = (node != nil ? node.attributes['port'].to_i : 8080)
 			end
+			# port
+			doc = XML::Parser.file('/etc/tomcat6/server.xml').parse
+			node = doc.find_first('/Server/Service/Connector')
+			@state["port"] = (node != nil ? node.attributes['port'].to_i : 8080)
 
 			return @state
 		end
