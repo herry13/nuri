ad4bb305b0e32383a39daf23a9ab927f573d15b3
herry <herry@nena.inf.ed.ac.uk>
2012-07-31 13:01:36 +0100

    update t0.rb

--- lib/sfp/dev/SFPParser.rb
+++ lib/sfp/dev/SFPParser.rb
@@ -5,7 +5,7 @@
 # Generated using ANTLR version: 3.2.1-SNAPSHOT Jul 31, 2010 19:34:52
 # Ruby runtime library version: 1.8.11
 # Input grammar file: SFP.g
-# Generated at: 2012-07-31 11:11:49
+# Generated at: 2012-07-31 13:01:02
 # 
 
 # ~~~> start load path setup
--- lib/sfp/dev/t0.rb
+++ lib/sfp/dev/t0.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+$: << File.expand_path(File.dirname(__FILE__) + "/../..")
+
 require './SFPParser'
 require './SFPLexer'
 require './sfp'
