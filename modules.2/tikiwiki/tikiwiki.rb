7b0a885f5cdda9c3eafd6cb4a791745676b36cc5
Herry <herry13@gmail.com>
2012-11-03 21:53:05 +0000

    update

--- modules.2/tikiwiki/tikiwiki.rb
+++ modules.2/tikiwiki/tikiwiki.rb
@@ -16,12 +16,28 @@ module Nuri
 			# get state of this component in JSON
 			def get_self_state
 				if File.file?(ConfigFile)
-					File.open(ConfigFile) do |f|
-						f.split("\n").each { |k,v| @state[k] = v }
-					end
+					config = read_config
+					config.each { |k,v| @state[k] = v }
 				end
 
 				return @state
+			end
+
+			def read_config
+				config = {}
+				File.open(ConfigFile) do |f|
+					f.split("\n").each { |line|
+						key, value = line.split('=', 2)
+						config[key] = value
+					}
+				end if File.file?(ConfigFile)
+				config
+			end
+
+			def write_config(config={})
+				File.open(ConfigFile, 'w') do |f|
+					config.each { |k,v| f.write(k + '=' + v + "\n") }
+				end
 			end
 	
 			def install(params={})
@@ -40,7 +56,9 @@ puts self.get('database').inspect
 
 			def set_webserver(params={})
 				Dir.mkdir(ConfigDir) if not File.directory?(ConfigDir)
-puts params['ws']
+				config = read_config
+				config['ws'] = params['ws']
+				write_config(config)
 				false
 			end
 
