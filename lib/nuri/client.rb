ef1353c1fb95faa75daac382c71bf87e056fc586
Herry <herry13@gmail.com>
2012-11-03 23:43:00 +0000

    update

--- lib/nuri/client.rb
+++ lib/nuri/client.rb
@@ -78,6 +78,7 @@ module Nuri
 						node = Nuri::Node.new(node)
 						value.each { |k,v| node.set(k,v) }
 						@daemon.root.set(node.name, node)
+puts @daemon.root.state.inspect
 						puts @daemon.root.get(key).name
 					end
 				end
