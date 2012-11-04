7c27975471f81edd83767034e6a69ab7ce43a9da
Herry <herry13@gmail.com>
2012-11-04 11:39:06 +0000

    update

--- lib/nuri/resource.rb
+++ lib/nuri/resource.rb
@@ -143,6 +143,7 @@ puts 'parent: ' + @parent.class.name
 				return @state
 			end
 
+			path.gsub!(/\./, '/') if path.isref
 			first, rest = path.split('/', 2)
 puts '@' + self.name + ' -- ' + first + ' :: ' + rest.to_s
 			if first == '$' or first == 'root'
