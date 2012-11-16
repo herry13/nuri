626582084f3c06acfca6914e61e5d5ea7849521f
Herry <herry13@gmail.com>
2012-11-16 15:33:19 +0000

    update

--- modules/tikidb/tikidb.rb
+++ modules/tikidb/tikidb.rb
@@ -32,52 +32,22 @@ module Nuri
 
 			def write_config(config={})
 				Dir.mkdir(ConfigDir) if not File.directory?(ConfigDir)
-				File.open(ConfigFile, 'w') do |f|
-					#config.each { |k,v| f.write(k + '=' + v.to_s + "\n") }
-					f.write( JSON.pretty_generate(config) )
-				end
+				File.open(ConfigFile, 'w') { |f| f.write( JSON.pretty_generate(config) ) }
 			end
 	
 			def install(params={})
 				return false if not Nuri::Util.installed?('mysql-client')
 
-				#web_host = self.get_state('tikiweb.parent.domainname')
-				#web_hosts = self.get_state('tikiweb')
-				mysql_port = self.get_state('database.port')
-				mysql_host = self.get_state('database.parent.domainname')
-				mysql_host = (mysql_host == Nuri::Util.domainname ? 'localhost' : mysql_host)
-				mysql_user = 'root'
-				mysql_password = self.get_state('database.root_password')
-				db_name = self.get_state('db_name')
-				db_user = self.get_state('db_user')
-				db_password = self.get_state('db_password')
+				db_name = self.get_state('db_name').to_s
 
-
-				if mysql_port != nil and mysql_host != nil and mysql_user != nil and mysql_password != nil and
-						db_name != nil and db_user != nil and db_password != nil
+				if db_name != nil and db_name != ''
 
 					sql = "DROP DATABASE IF EXISTS #{db_name}; CREATE DATABASE #{db_name} default character set 'UTF8'; "
-=begin
-					#web_hosts.each { |host|
-					#	sql += "GRANT ALL ON #{db_name}.* TO '#{db_user}'@'#{host}' IDENTIFIED BY '#{db_password}';"
-					#}
-					script_file = '/tmp/tikidb.sql'
-					File.open(script_file, 'w') do |file|
-						file.write(sql)
-						file.flush
-					end
-
-					cmd_mysql = "mysql --user=#{mysql_user} --password=#{mysql_password} --host=#{mysql_host} --port=#{mysql_port}"
-
-					cmd = "#{cmd_mysql} < #{script_file}"
-					return false if ( system(cmd) != true )
-					File.delete(script_file)
-=end
 					return false if not self.execute_sql(sql)
 
 					tikisql_file = Nuri::Util.home_dir + "/modules/tikidb/tiki.sql"
-					cmd = "#{cmd_mysql} #{db_name} < #{tikisql_file}"
-					return false if ( system(cmd) != true )
+					sql = File.read(tikisql_file)
+					return false if not self.execute_sql(sql)
 
 					config = self.read_config
 					config['installed'] = true
@@ -89,7 +59,13 @@ module Nuri
 				false
 			end
 
-			def execute_sql(user, passwd, host, port, sql)
+			def execute_sql(sql)
+				port = self.get_state('database.port')
+				host = self.get_state('database.parent.domainname')
+				host = (mysql_host == Nuri::Util.domainname ? 'localhost' : mysql_host)
+				user = 'root'
+				passwd = self.get_state('database.root_password')
+
 				script_file = '/tmp/tikidb.sql'
 				result = false
 				File.open(script_file, 'w') do |file|
@@ -105,28 +81,11 @@ module Nuri
 			def uninstall(params={})
 				return false if not Nuri::Util.installed?('mysql-client')
 
-				mysql_port = self.get_state('database.port')
-				mysql_host = self.get_state('database.parent.domainname')
-				mysql_host = (mysql_host == Nuri::Util.domainname ? 'localhost' : mysql_host)
-				mysql_user = 'root'
-				mysql_password = self.get_state('database.root_password')
 				db_name = self.get_state('db_name')
 
-				if mysql_port != nil and mysql_host != nil and mysql_user != nil and mysql_password != nil and db_name != nil 
-
-					sql = "DROP DATABASE IF EXISTS #{db_name}"
+				if db_name != nil 
+					sql = "DROP DATABASE IF EXISTS #{db_name};"
 					return false if not self.execute_sql(sql)
-=begin
-					script_file = '/tmp/tikidb.sql'
-					File.open(script_file, 'w') do |file|
-						file.write(sql)
-						file.flush
-					end
-
-					cmd = "mysql --user=#{mysql_user} --password=#{mysql_password} --host=#{mysql_host} --port=#{mysql_port} < #{script_file}"
-					return false if ( system(cmd) != true )
-					File.delete(script_file)
-=end
 
 					config = self.read_config
 					config['installed'] = false
@@ -144,26 +103,12 @@ module Nuri
 				db_name = self.get_state('db_name')
 				db_user = self.get_state('db_user')
 				db_password = self.get_state('db_password')
-				sql = "REVOKE ALL ON #{db_name}.* FROM '#{db_user}'@'%'";
+				sql = "REVOKE ALL ON #{db_name}.* FROM '#{db_user}'@'%';";
 
 				# 2) grant permissions from current tikiweb
 				params['webs'].each.each { |host|
 					sql += "GRANT ALL ON #{db_name}.* TO '#{db_user}'@'#{host}' IDENTIFIED BY '#{db_password}';"
 				}
-=begin
-				script_file = '/tmp/tikidb.sql'
-				File.open(script_file, 'w') do |file|
-					file.write(sql)
-					file.flush
-				end
-
-				# execute the SQL
-				cmd_mysql = "mysql --user=#{mysql_user} --password=#{mysql_password} --host=#{mysql_host} --port=#{mysql_port}"
-
-				cmd = "#{cmd_mysql} < #{script_file}"
-				return false if ( system(cmd) != true )
-				File.delete(script_file)
-=end
 
 				config = self.read_config
 				if not self.execute_sql(sql)
