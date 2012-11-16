78dcd08f5bf947c771953cc2eefbc9df7f6b2deb
Herry <herry13@gmail.com>
2012-11-16 15:15:13 +0000

    update

--- modules/tikidb/tikidb.rb
+++ modules/tikidb/tikidb.rb
@@ -41,7 +41,8 @@ module Nuri
 			def install(params={})
 				return false if not Nuri::Util.installed?('mysql-client')
 
-				web_host = self.get_state('tikiweb.parent.domainname')
+				#web_host = self.get_state('tikiweb.parent.domainname')
+				#web_hosts = self.get_state('tikiweb')
 				mysql_port = self.get_state('database.port')
 				mysql_host = self.get_state('database.parent.domainname')
 				mysql_host = (mysql_host == Nuri::Util.domainname ? 'localhost' : mysql_host)
@@ -56,7 +57,10 @@ module Nuri
 						db_name != nil and db_user != nil and db_password != nil
 
 					sql = "DROP DATABASE IF EXISTS #{db_name}; CREATE DATABASE #{db_name} default character set 'UTF8'; "
-					sql += "GRANT ALL ON #{db_name}.* TO '#{db_user}'@'#{web_host}' IDENTIFIED BY '#{db_password}';"
+=begin
+					#web_hosts.each { |host|
+					#	sql += "GRANT ALL ON #{db_name}.* TO '#{db_user}'@'#{host}' IDENTIFIED BY '#{db_password}';"
+					#}
 					script_file = '/tmp/tikidb.sql'
 					File.open(script_file, 'w') do |file|
 						file.write(sql)
@@ -68,6 +72,8 @@ module Nuri
 					cmd = "#{cmd_mysql} < #{script_file}"
 					return false if ( system(cmd) != true )
 					File.delete(script_file)
+=end
+					return false if not self.execute_sql(sql)
 
 					tikisql_file = Nuri::Util.home_dir + "/modules/tikidb/tiki.sql"
 					cmd = "#{cmd_mysql} #{db_name} < #{tikisql_file}"
@@ -81,6 +87,19 @@ module Nuri
 				end
 
 				false
+			end
+
+			def execute_sql(user, passwd, host, port, sql)
+				script_file = '/tmp/tikidb.sql'
+				result = false
+				File.open(script_file, 'w') do |file|
+					file.write(sql)
+					file.flush
+				end
+				cmd = "mysql --user=#{user} --password=#{passwd} --host=#{host} --port=#{port} < #{script_file}"
+				result = system(cmd)
+				File.delete(script_file)
+				return (result == true)
 			end
 		
 			def uninstall(params={})
@@ -96,6 +115,8 @@ module Nuri
 				if mysql_port != nil and mysql_host != nil and mysql_user != nil and mysql_password != nil and db_name != nil 
 
 					sql = "DROP DATABASE IF EXISTS #{db_name}"
+					return false if not self.execute_sql(sql)
+=begin
 					script_file = '/tmp/tikidb.sql'
 					File.open(script_file, 'w') do |file|
 						file.write(sql)
@@ -105,6 +126,7 @@ module Nuri
 					cmd = "mysql --user=#{mysql_user} --password=#{mysql_password} --host=#{mysql_host} --port=#{mysql_port} < #{script_file}"
 					return false if ( system(cmd) != true )
 					File.delete(script_file)
+=end
 
 					config = self.read_config
 					config['installed'] = false
@@ -118,10 +140,41 @@ module Nuri
 			end
 
 			def set_tikiweb(params={})
+				# 1) revoke previous permissions
+				db_name = self.get_state('db_name')
+				db_user = self.get_state('db_user')
+				db_password = self.get_state('db_password')
+				sql = "REVOKE ALL ON #{db_name}.* FROM '#{db_user}'@'%'";
+
+				# 2) grant permissions from current tikiweb
+				params['webs'].each.each { |host|
+					sql += "GRANT ALL ON #{db_name}.* TO '#{db_user}'@'#{host}' IDENTIFIED BY '#{db_password}';"
+				}
+=begin
+				script_file = '/tmp/tikidb.sql'
+				File.open(script_file, 'w') do |file|
+					file.write(sql)
+					file.flush
+				end
+
+				# execute the SQL
+				cmd_mysql = "mysql --user=#{mysql_user} --password=#{mysql_password} --host=#{mysql_host} --port=#{mysql_port}"
+
+				cmd = "#{cmd_mysql} < #{script_file}"
+				return false if ( system(cmd) != true )
+				File.delete(script_file)
+=end
+
 				config = self.read_config
-				config['tikiweb'] = params['webs']
-				self.write_config(config)
-				true
+				if not self.execute_sql(sql)
+					config['tikiweb'] = []
+					self.write_config(config)
+					return false
+				else
+					config['tikiweb'] = params['webs']
+					self.write_config(config)
+					return true
+				end
 			end
 
 			def set_account(params={})
--- modules/tikiweb/tikiweb.rb
+++ modules/tikiweb/tikiweb.rb
@@ -8,7 +8,8 @@ module Nuri
 
 			ConfigDir = '/var/lib/tikiweb'
 			ConfigFile = '/var/lib/tikiweb/config'
-			DownloadURL = 'http://sourceforge.net/projects/tikiwiki/files/Tiki_9.x_Herbig_Haro/9.2/tiki-9.2.tar.gz'
+			DownloadURL = 'http://nena.inf.ed.ac.uk/tikiwiki/tiki-9.2.tar.gz'
+			#DownloadURL = 'http://sourceforge.net/projects/tikiwiki/files/Tiki_9.x_Herbig_Haro/9.2/tiki-9.2.tar.gz'
 
 			def initialize
 				self.register('Tikiweb', 'tikiweb')
