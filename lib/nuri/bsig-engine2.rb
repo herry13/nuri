3441bffae9df99c6d2beaf718656e7cdf27564b1
Herry <herry13@gmail.com>
2013-05-27 16:51:48 +0100

    replace bsig_engine.rb with bsig_engine2.rb

--- lib/nuri/bsig-engine2.rb
+++ lib/nuri/bsig-engine2.rb
@@ -22,6 +22,10 @@ module Operator
 		self['distance'].to_i
 	end
 
+	def conditions
+		self['condition']
+	end
+
 	# @param goal => a Hash contains the goal state
 	#
 	# @return true if this operator supports the goal state, otherwise false
@@ -123,7 +127,7 @@ Nuri::Util.debug "#{operator['name']} => #{operator.selected}"
 			operator.selected = true
 		}
 
-		local, remote = self.split_conditions(operator)
+		local, remote = self.split_conditions(operator.conditions)
 
 		status = nil
 		begin
