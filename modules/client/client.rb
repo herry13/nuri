54edffef41ad2a22d9defc3b8f8ae725ef4c38b2
Herry <herry13@gmail.com>
2013-08-04 08:44:16 +0100

    fix module client

--- modules/client/client.rb
+++ modules/client/client.rb
@@ -1,24 +1,22 @@
-module Sfp::Module
-	class Cloud
-		include Sfp::Resource
+class Sfp::Module::Client
+	include Sfp::Resource
 
-		ConfigFile = '/tmp/sfp_client_config'
-		def update_state
-			if File.exist?(ConfigFile)
-				@state['refer'] = File.read(ConfigFile)
-			else
-				@state['refer'] = nil
-			end
+	ConfigFile = '/tmp/sfp_client_config'
+	def update_state
+		if File.exist?(ConfigFile)
+			@state['refer'] = File.read(ConfigFile)
+		else
+			@state['refer'] = nil
 		end
+	end
 
-		def redirect(p={})
-			return false if !p.has_key?('s')
-			if p['s'].nil?
-				File.delete(ConfigFile) if File.exist?(ConfigFile)
-			else
-				File.open(ConfigFile, 'w') { |f| f.write(p['s'].to_s) }
-			end
-			true
+	def redirect(p={})
+		return false if !p.has_key?('s')
+		if p['s'].nil?
+			File.delete(ConfigFile) if File.exist?(ConfigFile)
+		else
+			File.open(ConfigFile, 'w') { |f| f.write(p['s'].to_s) }
 		end
+		true
 	end
 end
