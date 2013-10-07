580deb8890c148e1dda9aafd292d01f1d9157c86
herry13 <herry13@gmail.com>
2013-10-07 17:04:45 +0100

    module machine: implement disk specification

--- modules/machine/machine.rb
+++ modules/machine/machine.rb
@@ -26,5 +26,72 @@ class Sfp::Module::Machine
 
 		@state["cpus"] = (File.exist?('/proc/cpuinfo') ? `cat /proc/cpuinfo | grep processor | wc -l`.strip.to_i : -1)
 		@state['memory'] = (`which free`.strip != '' ? `free`.split("\n")[1].split(" ")[1] : -1)
+
+		@state['disk'] = get_disk_state
+	end
+
+	protected
+	# generate the disks' state, try to automatically mount the disk to target directory
+	#
+	def get_disk_state
+		def generate_state
+			disk = {}
+			# get disks UUID
+			uuids = {}
+			`blkid`.each_line do |line|
+				line.strip!
+				next if line.length <= 0
+				device, info = line.split(':', 2)
+				info = info.split(' ')
+				uuids[device] = info[0].split('=', 2)[1].gsub(/"/, '')
+			end
+
+			# read fstab
+			`df -l`.each_line do |line|
+				line.strip!
+				next if line.length <= 0
+				data = line.split(' ')
+				if data[0][0..4] == '/dev/'
+					name = 'root'
+					if data[5] != '/'
+						model = (@model['disk'].is_a?(Hash) ? @model['disk'].select { |k,v| v['mount'] == data[5] if k[0] != '_' } : {})
+						name = (model.length > 0 ? model.keys.first : "uuid_#{uuids[data[0]]}")
+					end
+					disk[name] = {
+						'size' => (data[1].to_f / 1000.0).to_i,
+						'mount' => data[5],
+						'uuid' => uuids[data[0]]
+					}
+				end
+			end
+			disk
+		end
+
+		disk = generate_state
+		if @model['disk'].is_a?(Hash)
+			names = @model['disk'].keys.sort { |x,y| x <=> y }
+			device = "/dev/vdb"
+			names.each { |name|
+				next if name[0] == '_' or disk.has_key?(name)
+				spec = @model['disk'][name]
+				status = `file -s #{device}`
+				if not (status =~ /ERROR/)
+					target = spec['mount'].to_s.strip
+					# format the disk if not yet formatted
+					system("mkfs.ext4 #{device}") if not (status =~ /.+ filesystem data/)
+					# create target directory if not exist
+					system("mkdir -p #{target}") if !File.exist?(target)
+					# mount the disk
+					if target.length > 0 and system("mount #{device} #{target}")
+						Sfp::Agent.logger.info "mount #{device} to #{target}  [OK]"
+					else
+						Sfp::Agent.logger.info "mount #{device} to #{target}  [Failed]"
+					end
+				end
+				device = device.next
+			}
+		end
+
+		generate_state
 	end
 end
