39fb7d0a8d593188d520f6808a35acdc672aea49
root <root@master.nuri.ext9.sup.hpl.hp.com>
2013-12-09 07:01:31 +0000

    fix minor bugs

--- modules/vm/vm.rb
+++ modules/vm/vm.rb
@@ -1,8 +1,10 @@
 require File.expand_path(File.dirname(__FILE__)) + "/../machine/machine.rb"
 
 class Sfp::Module::VM < Sfp::Module::Machine
+	InCloud = "#{Sfp::Agent::Home}/vm.in_cloud"
+
 	def update_state
 		self.class.superclass.instance_method(:update_state).bind(self).call
-		@state['in_cloud'] = @model['in_cloud']
+		@state['in_cloud'] = (File.exist?(InCloud) ? File.read(InCloud).strip : '')
 	end
 end
