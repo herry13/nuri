a117de04cb231c73076456fc4fe2545490ee2152
Herry <herry13@gmail.com>
2014-04-10 12:17:42 +0100

    update module os: enable/disable auto_upgrade (ubuntu/debian only)

--- modules/os/os.rb
+++ modules/os/os.rb
@@ -1,12 +1,13 @@
 require 'rubygems'
 require 'json'
+require 'digest/sha1'
 
 class Sfp::Module::OS
 	include Sfp::Resource
 
 	def update_state
 		@state['running'] = true
-		#@state['auto_upgrade'] = @model['auto_upgrade']
+		@state['auto_upgrade'] = auto_upgrade?
 
 		# get memory info
 		if `which free`.strip != ''
@@ -34,5 +35,30 @@ class Sfp::Module::OS
 
 	def stop(p={})
 		return !!system('/sbin/shutdown -h now')
+	end
+
+	protected
+	AutoUpgradeCronFile = "/etc/cron.daily/nuri.module.os"
+	DebianAutoUpgradeCronFileContent = "#!/bin/bash\n\napt-get update -y && apt-get upgrade -y"
+	def auto_upgrade?
+		def apt
+			begin
+				current = (::File.exist?(AutoUpgradeCronFile) ? Digest::SHA1.hexdigest(::File.read(AutoUpgradeCronFile)) : '')
+				desired = Digest::SHA1.hexdigest(DebianAutoUpgradeCronFileContent)
+				File.open(AutoUpgradeCronFile, 'w') do |f|
+					f.write DebianAutoUpgradeCronFileContent
+					FileUtils.chmod 0755, AutoUpgradeCronFile
+				end if current != desired
+				return true
+			rescue Exception => e
+				log.error "cannot create cron daily #{e}"
+			end
+			false
+		end
+
+		if @model['auto_upgrade']
+			return apt if `which apt-get`.strip != ''
+		end
+		false
 	end
 end
